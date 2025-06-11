50fb2d667903ac4235faff305d1367832727a853
Hasancan Sahan <yamahasirossi@gmail.com>
2025-06-11 21:27:49 +0300

    added test codes

--- TCPWatch/src/main/java/org/TCPWatchExample/Client/Main.java
+++ TCPWatch/src/main/java/org/TCPWatchExample/Client/Main.java
@@ -5,19 +5,28 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         var socket = new TCPSocketClient("localhost",9090);
-        socket.watchDelete();
+        var socket2 = new TCPSocketClient("localhost",9090);
 
+        socket.watch("/service/java-app-1");
+        socket2.watch("/service/java-app-1");
         var msg = socket.sendMessage("PING");
         System.out.println(msg);
-
-        msg = socket.sendMessage("PUT 1 MERHABA");
+        msg = socket.sendMessage("PUT /service/java-app-1 127.0.0.1");
         System.out.println(msg);
-        msg = socket.sendMessage("GET 1");
+        msg = socket.sendMessage("GET /service/java-app-1");
         System.out.println(msg);
-        socket.sendMessage("REMOVE 1");
+        socket.sendMessage("REMOVE /service/java-app-1");
+        msg = socket.sendMessage("PUT /service/java-app-1 143.10.55.199");
+        System.out.println(msg);
 
-//        socket.sendMessage("EXIT");
-//        socket.closeConnection();
+        msg = socket.sendMessage("PUT /service/java-app-1 143.10.55.119");
+        System.out.println(msg);
 
+        msg = socket.sendMessage("GET /service/java-app-1");
+        System.out.println(msg);
+        socket.sendMessage("REMOVE /service/java-app-1");
+
+        socket.closeEventListenerConnection();
+        socket.closeConnection();
     }
 }
