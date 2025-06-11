3eb2b6e227e3c479ff12721cdc7548e51f6c46f5
Hasancan Sahan <yamahasirossi@gmail.com>
2025-06-11 21:29:48 +0300

    Update ObservableCache.java

--- TCPWatch/src/main/java/org/TCPWatchExample/Server/ObservableCache.java
+++ TCPWatch/src/main/java/org/TCPWatchExample/Server/ObservableCache.java
@@ -4,37 +4,61 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.List;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class ObservableCache {
 
-    private final ConcurrentNavigableMap<Integer, String> cache;
-    private final List<ClientSession> observers;
-    public ObservableCache(ConcurrentNavigableMap<Integer, String> cache){
+    private final ConcurrentNavigableMap<String, String> cache;
+    private final ConcurrentMap<String ,List<ClientSession>> observers;
+    public ObservableCache(ConcurrentNavigableMap<String, String> cache, ConcurrentMap<String ,List<ClientSession>> watchKeyList){
         this.cache = cache;
-        this.observers = new CopyOnWriteArrayList<>();
+        this.observers = watchKeyList;
     }
-    public void addObserver(ClientSession session) throws IOException {
-        observers.add(session);
-    }
-    private void notifyObserversWhenItemDeleted(Integer key, String value) {
-        observers.forEach((socket) -> {
-            socket.send("KEY : " + key + " -- VALUE : " + value + " deleted !!");
+    public void addObserver(String key, ClientSession session) throws IOException {
+        observers.compute(key, (k,v) -> {
+            if(v == null){
+                List<ClientSession> list = new CopyOnWriteArrayList<>();
+                list.add(session);
+                return list;
+            }
+            v.add(session);
+            return v;
         });
     }
-    public String get(Integer key) {
+    private void notifyObservers(List<ClientSession> cs, Consumer<ClientSession> cb ) {
+        cs.forEach(cb);
+    }
+    public String get(String key) {
         return cache.get(key);
     }
 
-    public String put(Integer key, String val) {
+    public String put(String key, String val) {
 
-        return cache.put(key, val);
+
+        String updatedItem = cache.put(key, val);
+        if(observers.containsKey(key)) {
+            var observerList = observers.get(key);
+
+            notifyObservers(observerList,(socket) -> {
+                socket.send(key + " updated with "+ val);
+            });
+        }
+        return updatedItem;
     }
 
-    public String remove(Integer key) {
+    public String remove(String key) {
         String deletedItem =  cache.remove(key);
-        notifyObserversWhenItemDeleted(key, deletedItem);
+        if(observers.containsKey(key)) {
+            var observerList = observers.get(key);
+
+            notifyObservers(observerList,(socket) -> {
+                socket.send(key + " deleted");
+            });
+        }
         return deletedItem;
     }
 }
